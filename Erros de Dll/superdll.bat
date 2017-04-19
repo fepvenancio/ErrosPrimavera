587c26aa0e9c7778eef977dcac6fae7eb7593b8d
luCyfahh <filipe.venancio@fixus.co.ao>
2017-04-19 15:08:09 +0100

    Add files via upload

---
+++ Erros de Dll/superdll.bat
@@ -0,0 +1,133 @@
+:Application-defined or object-defined error  
+
+
+:plataforma
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdUp900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\CRUFLPriGlobal900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\InstPFR800Engine900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\OfaAudit900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\OfaForms900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\PriImpExpConf900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\PriTextBoxF4900.ocx"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\Primavera.Platform.Caching900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdBackgrWorker900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdBrandingInfo900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdChkTreeView900.ocx"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdCommonControls900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\IInstAdmEngine900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdControls900.ocx"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdCtlPainelGestao900.ocx"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdDTPicker900.ocx"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdPaneControls900.ocx"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdPlatMapas900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdServices900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdStatusBar900.ocx"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdUpgCDUs900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\AdmAudit900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\AdmBE900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\IInstAdmUserExits900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\AdmBS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\AdmDS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\AdmEngine900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdBE900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdBESql900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdBSSql900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdClasses900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdDS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdDSSql900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdImpWiz900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\IPriImpExpConf900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdPlatBE900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdPlatBS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdPlatDS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdUpgPlat900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdVbaControls900.ocx"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdVbaExt900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdNetIntfCtls.ocx"
+regsvr32 /s "C:\Windows\SysWOW64\vbalExpBar6.ocx"
+regsvr32 /s "C:\Windows\SysWOW64\StdMaskNumCtl.ocx"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\BaseDadosInfo900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\InstAdmBE900.dll"
+regsvr32 /s "C:\Windows\SysWOW64\winhttp.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\MSSoap\Binaries\MSSOAP30.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\MSSoap\Binaries\SOAPIS30.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\MSSoap\Binaries\WHSC30.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\MSSoap\Binaries\WISC30.dll"
+regsvr32 /s "C:\Windows\SysWOW64\msxml2.dll"
+regsvr32 /s "C:\Windows\SysWOW64\msxml4.dll"
+regsvr32 /s "C:\Program Files (x86)\PRIMAVERA\SG900\Apl\InstAdmUS900.exe"
+regsvr32 /s "C:\Program Files (x86)\PRIMAVERA\SG900\Apl\Adm900LE.exe"
+regsvr32 /s "C:\Program Files (x86)\PRIMAVERA\SG900\Apl\PriBak900LE.exe"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\InstAdmBS900.dll"
+regsvr32 /s "C:\Program Files (x86)\PRIMAVERA\SG900\Apl\LicenseLoader900.exe"
+regsvr32 /s "C:\Program Files (x86)\PRIMAVERA\SG900\Apl\ePriImport.exe"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\InstAdmDS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\InstERPEngine900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\InstPFR160Engine900.dll"
+
+:rhp
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdUp900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\IRhpBS900.tlb"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\RhpForms900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\RhpPrint900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\RhpUS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\RhpUp900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\BaseDadosInfo900.dll"
+regsvr32 /s "C:\Program Files (x86)\PRIMAVERA\SG900\Apl\Bls900LE.exe"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\IRhpDS900.tlb"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\PriImpExpRHP900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\RhpAudit900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\RhpBE900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\RhpBS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\RhpBSSP900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\RhpDS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\RhpDSSP900.dll"
+
+:eap
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdUP900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\IEapBS900.tlb"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\BaseDadosInfo900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\IEapDS900.tlb"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\EapAudit900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\EapBE900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\EapBS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\EapDS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\EapForms900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\EapUS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\EapUp900.dll"
+
+:gcp
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\GcpSchedule.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\StdUp900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\GcpBS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\GcpDS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\GcpForms900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\GcpUS900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\GcpUp900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\BaseDadosInfo900.dll"
+regsvr32 /s "C:\Program Files (x86)\PRIMAVERA\SG900\Apl\POS900LE.exe"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\BalCachapuz.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\BalRuby.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\GcpExp900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\GcpTab900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\IGcpBS900.tlb"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\IGcpDS900.tlb"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\GcpAudit900.dll"
+regsvr32 /s "C:\Program Files (x86)\Common Files\PRIMAVERA\SG900\GcpBE900.dll"
+
+cd  %SYSTEMROOT%\system32
+cd  %SYSTEMROOT%\syswow64
+
+
+for %%f in (codej*.ocx ) do regsvr32 /s %%f
+
+set lista=( Ecdsacom.dll scrrun.dll msvbvm60.dll sqldmo.dll SSubTmr6.dll vbalExpBar6.ocx vbalIml6.ocx MSCOMCTL.OCX mdiextender.ocx MSVBVM60.dll msxml3.dll msxml4.dll msxml6.dll xarray32.ocx xceedzip.ocx)
+for %%i in %lista% do regsvr32 /s %%i
+
+
+cd  %ProgramFiles%\Common Files\System\ado\ 
+cd  %ProgramFiles(x86)%\Common Files\System\ado\
+
+set ADO=( msado15.dll msadomd.dll msador15.dll msadox.dll msadrh15.dll msjro.dll )
+for %%i in %ADO% do regsvr32 /s %%i
+
